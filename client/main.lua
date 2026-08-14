0af91a4d77ef9dd3bdf50eb993ee7bc8f7641065
Hanna <hannaseraphim@gmail.com>
2026-08-14 02:14:55 -0400

    refactor(lua): update threads logic

--- client/main.lua
+++ client/main.lua
@@ -1,38 +1,80 @@
+local isInsideVehicle = false
+local minimap = nil
+
+-- -------------------------------------------------------------------------- --
+--                                   Startup                                  --
+-- -------------------------------------------------------------------------- --
+CreateThread(function()
+    minimap = RequestScaleformMovie("minimap")
+    SetRadarBigmapEnabled(true, false)
+    SetRadarBigmapEnabled(false, false)
+end)
+
+-- -------------------------------------------------------------------------- --
+--                               HUD Settings                                 --
+-- -------------------------------------------------------------------------- --
 CreateThread(function()
     while true do
-        local ped = PlayerPedId()
-        local inVehicle = IsPedInAnyVehicle(ped, false)
+        if isInsideVehicle then
+            DisplayRadar(true)
+            SetRadarZoom(0)
 
-        if inVehicle then
-            SendNUIMessage({ action = 'setSpeedometerVisibility', data = true })
+            BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
+            ScaleformMovieMethodAddParamInt(3)
+            EndScaleformMovieMethod()
         else
-            SendNUIMessage({ action = 'setSpeedometerVisibility', data = false })
+            DisplayRadar(false)
+        end
+
+        HideHudComponentThisFrame(6)
+        HideHudComponentThisFrame(7)
+        HideHudComponentThisFrame(8)
+        HideHudComponentThisFrame(9)
+
+        Wait(0)
+    end
+end)
+
+-- -------------------------------------------------------------------------- --
+--                              Vehicle detection                             --
+-- -------------------------------------------------------------------------- --
+CreateThread(function()
+    while true do
+        local inVehicle = IsPedInAnyVehicle(PlayerPedId(), false)
+
+        if inVehicle ~= isInsideVehicle then
+            isInsideVehicle = inVehicle
+            SendNUIMessage({ action = 'setSpeedometerVisibility', data = inVehicle })
         end
 
         Wait(500)
     end
 end)
 
-
+-- -------------------------------------------------------------------------- --
+--                               Speedometer                                  --
+-- -------------------------------------------------------------------------- --
 CreateThread(function()
     while true do
-        local ped = PlayerPedId()
-        local veh = GetVehiclePedIsIn(ped, false)
-        
-        if IsPedInAnyVehicle(ped, false) then
-            SetNuiFocus(false, false)
-            SendNUIMessage({
-                action = 'updateSpeedometer',
-                data = {
-                    speed = GetEntitySpeed(veh) * 3.6,
-                    rpm = GetVehicleCurrentRpm(veh),
-                    gear = GetVehicleCurrentGear(veh),
-                    fuel = GetVehicleFuelLevel(veh) / 100,
-                    engineOn = GetIsVehicleEngineRunning(veh),
-                    engineHealth = GetVehicleEngineHealth(veh) / 1000
-                }
-            })
+        if isInsideVehicle then
+            local ped = PlayerPedId()
+            local veh = GetVehiclePedIsIn(ped, false)
+
+            if DoesEntityExist(veh) then
+                SendNUIMessage({
+                    action = 'updateSpeedometer',
+                    data = {
+                        speed        = GetEntitySpeed(veh) * 3.6,
+                        rpm          = GetVehicleCurrentRpm(veh),
+                        gear         = GetVehicleCurrentGear(veh),
+                        fuel         = GetVehicleFuelLevel(veh) / 100,
+                        engineOn     = GetIsVehicleEngineRunning(veh),
+                        engineHealth = GetVehicleEngineHealth(veh) / 1000,
+                    }
+                })
+            end
         end
+
         Wait(50)
     end
 end)
